01d1d4ada23882e5a0027cfeef1c96f4966f4ed8
Manel Matougui <m.matougui@gmail.com>
2018-09-01 23:06:56 -0700

    add mapview

--- VirtualTourist/VirtualTourist/TravelLocationsMapView.swift
+++ VirtualTourist/VirtualTourist/TravelLocationsMapView.swift
@@ -7,9 +7,11 @@
 //
 
 import UIKit
+import MapKit
 
-class TravelLocationsMapView: UIViewController {
+class TravelLocationsMapView: UIViewController, MKMapViewDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +20,25 @@ class TravelLocationsMapView: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        let reuseId = "pin"
+        
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKPinAnnotationView
+        
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView!.canShowCallout = true
+            pinView!.pinColor = .red
+            pinView!.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+        }
+        else {
+            pinView!.annotation = annotation
+        }
+        
+        return pinView
     }
 
 
