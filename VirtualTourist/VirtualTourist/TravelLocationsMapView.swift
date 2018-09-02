edc1ce04249bbed9af0bcf4fc2ced81775b174aa
Manel Matougui <m.matougui@gmail.com>
2018-09-01 22:54:21 -0700

    add TravelLocationsMapViewController

---
+++ VirtualTourist/VirtualTourist/TravelLocationsMapView.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VirtualTourist
+//
+//  Created by Manel matougui on 9/1/18.
+//  Copyright © 2018 udacity. All rights reserved.
+//
+
+import UIKit
+
+class TravelLocationsMapView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
