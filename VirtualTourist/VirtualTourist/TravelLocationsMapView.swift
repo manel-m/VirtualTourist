4c4fc3a4143b4bebca1980bf7639de6b2b1a653d
Manel Matougui <m.matougui@gmail.com>
2018-09-02 23:03:39 -0700

    add add pin

--- VirtualTourist/VirtualTourist/TravelLocationsMapView.swift
+++ VirtualTourist/VirtualTourist/TravelLocationsMapView.swift
@@ -8,13 +8,44 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class TravelLocationsMapView: UIViewController, MKMapViewDelegate {
+class TravelLocationsMapView: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+//        if #available(iOS 8.0, *) {
+//            locationManager.requestAlwaysAuthorization()
+//        } else {
+//            // Fallback on earlier versions
+//        }
+        locationManager.startUpdatingLocation()
+        // add gesture recognizer
+       
+        let longPress = UILongPressGestureRecognizer(target: self, action: #selector(TravelLocationsMapView.mapLongPress(_:))) // colon needs to pass through info
+        longPress.minimumPressDuration = 1.5 // in seconds
+        //add gesture recognition
+        mapView.addGestureRecognizer(longPress)
+    }
+    
+    @objc func mapLongPress(_ recognizer: UIGestureRecognizer) {
+        
+        print("A long press has been detected.")
+        
+        let touchedAt = recognizer.location(in: self.mapView) // adds the location on the view it was pressed
+        let touchedAtCoordinate : CLLocationCoordinate2D = mapView.convert(touchedAt, toCoordinateFrom: self.mapView) // will get coordinates
+        
+        let newPin = MKPointAnnotation()
+        newPin.coordinate = touchedAtCoordinate
+        mapView.addAnnotation(newPin)
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,25 +53,6 @@ class TravelLocationsMapView: UIViewController, MKMapViewDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-        
-        let reuseId = "pin"
-        
-        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKPinAnnotationView
-        
-        if pinView == nil {
-            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
-            pinView!.canShowCallout = true
-            pinView!.pinColor = .red
-            pinView!.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
-        }
-        else {
-            pinView!.annotation = annotation
-        }
-        
-        return pinView
-    }
-
-
+    
 }
 
