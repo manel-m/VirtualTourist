ad69000dcdfa2373e8104a2c110a2ed81e93e066
Manel Matougui <m.matougui@gmail.com>
2018-10-07 21:00:32 -0700

    remove Photos from persist store and collection View

--- VirtualTourist/VirtualTourist/ViewControllers/PhotoAlbumView.swift
+++ VirtualTourist/VirtualTourist/ViewControllers/PhotoAlbumView.swift
@@ -10,22 +10,25 @@ import Foundation
 import UIKit
 import MapKit
 import CoreData
-class PhotoAlbumView: UIViewController ,UICollectionViewDataSource , UICollectionViewDelegate {
+class PhotoAlbumView: UIViewController ,UICollectionViewDataSource , UICollectionViewDelegate, NSFetchedResultsControllerDelegate {
     
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var flawLayout: UICollectionViewFlowLayout!
     
     var annotation : MKAnnotation?
-    var imageData : [Data] = []
+//    var imageData : [Data] = []
     var dataController:DataController!
-    var photo: Photo!
+    //var photo: Photo!
     var pin : Pin!
     var photos:[Photo] = []
-//    var fetchedResultsController:NSFetchedResultsController<Photo>!
+   // var fetchedResultsController:NSFetchedResultsController<Photo>!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.collectionView.delegate = self
+        
         // flowLayout
         let space: CGFloat = 3.0
         let dimension = (view.frame.size.width - (2 * space)) / 3.0
@@ -46,34 +49,48 @@ class PhotoAlbumView: UIViewController ,UICollectionViewDataSource , UICollectio
     
         print("photos in store \(self.photos.count)")
         if self.photos.count == 0 {
-            // Call API
-            searchByLatLon(latitude: (annotation?.coordinate.latitude)!, longitude: (annotation?.coordinate.longitude)!) { (results) in
-                self.imageData = results
-                for image in self.imageData {
-                    let photo = Photo(context: self.dataController.viewContext)
-                    photo.creationDate = Date()
-                    photo.photoData = image
-                    photo.pin = self.pin
-                    try? self.dataController.viewContext.save()
-                }
-                
-                performUIUpdatesOnMain {
-                    self.collectionView.reloadData()
-                }
-            }
+            self.getFromFlicker()
         } else {
-            for photo in photos {
-                let data = photo.photoData!
-                self.imageData.append(data)
-            }
-            
             self.collectionView.reloadData()
         }
-
     }
+    
+    func getFromFlicker() {
+        // Call API
+        searchByLatLon(latitude: (annotation?.coordinate.latitude)!, longitude: (annotation?.coordinate.longitude)!) { (results) in
+            for image in results {
+                let photo = Photo(context: self.dataController.viewContext)
+                photo.creationDate = Date()
+                photo.photoData = image
+                photo.pin = self.pin
+                try? self.dataController.viewContext.save()
+                self.photos.append(photo)
+            }
+            
+            performUIUpdatesOnMain {
+                self.collectionView.reloadData()
+            }
+        }
+    }
+//    func setupFetchedResultsController() {
+//        let fetchRequest: NSFetchRequest<Photo> = Photo.fetchRequest()
+//        let predicate = NSPredicate(format: "pin == %@", pin)
+//        fetchRequest.predicate = predicate
+//        let sortDescriptor = NSSortDescriptor(key : "creationDate", ascending: false)
+//        fetchRequest.sortDescriptors = [sortDescriptor]
+//
+//        fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: dataController.viewContext, sectionNameKeyPath: nil, cacheName: nil)
+//        fetchedResultsController.delegate = self
+//        do {
+//            try fetchedResultsController.performFetch()
+//        } catch {
+//            fatalError("The fetch cannot be perfrmed: \(error.localizedDescription)")
+//        }
+//    }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
+        //setupFetchedResultsController()
         //Setting Region
         let center = CLLocationCoordinate2D(latitude: (annotation?.coordinate.latitude)!, longitude: (annotation?.coordinate.longitude)!)
         let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
@@ -88,10 +105,11 @@ class PhotoAlbumView: UIViewController ,UICollectionViewDataSource , UICollectio
         
 
     }
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        //fetchedResultsController = nil
-    }
+
+//    override func viewWillDisappear(_ animated: Bool) {
+//        super.viewWillDisappear(animated)
+//        fetchedResultsController = nil
+//    }
     
 
 
@@ -100,23 +118,36 @@ class PhotoAlbumView: UIViewController ,UICollectionViewDataSource , UICollectio
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         //print("imageData.count = \(self.imageData.count)")
         
-        return self.imageData.count
+        return self.photos.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoAlbumViewCell", for: indexPath) as! PhotoAlbumViewCell
-        let image = self.imageData[(indexPath as NSIndexPath).row]
+        let image = self.photos[(indexPath as NSIndexPath).row].photoData!
         // Set the image
         cell.photoImageView?.image = UIImage(data: image)
         
         return cell
     }
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath:IndexPath) {
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         
-//        let detailController = self.storyboard!.instantiateViewController(withIdentifier: "VillainDetailViewController") as! VillainDetailViewController
-//        detailController.villain = self.allVillains[(indexPath as NSIndexPath).row]
-//        self.navigationController!.pushViewController(detailController, animated: true)
+        print("select image \(indexPath.row)")
+//        self.imageData.remove(at: (indexPath as NSIndexPath).row)
+        
+        // delete image from in-memory array
+        let photo = photos[indexPath.row]
+        photos.remove(at: indexPath.row)
+        // delete image from persistent store
+        dataController.viewContext.delete(photo)
+        try? dataController.viewContext.save()
+
+        self.collectionView.reloadData()
+
+//        let photoToDelete = fetchedResultsController.object(at: indexPath)
+//        dataController.viewContext.delete(photoToDelete)
+//        try? dataController.viewContext.save()
         
     }
 }
